bacdf74abd40ca708ec9dc7d9ead16bbab18fe0e
Viacheslav Iftodii <158159514+SlavikBlxt4@users.noreply.github.com>
2025-11-01 16:58:45 +0100

    Update comment from 'prueba pipeline 3' to 'prueba pipeline 4'

--- services/decision/worker.py
+++ services/decision/worker.py
@@ -10,7 +10,7 @@ r = redis.Redis(host=REDIS_HOST, port=6379, db=0)
 
 def pg():
     return psycopg2.connect(PG_DSN)
-# prueba pipeline 3
+# prueba pipeline 4
 
 def previous_decision(evt):
     fp  = evt.get("fingerprint")
